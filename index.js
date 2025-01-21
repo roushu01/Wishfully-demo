f60d5b7baba0051c27d31d57015af2036b0be788
roushu01 <roushnikumar@gmail.com>
2025-01-21 23:27:50 +0530

    hello

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- 
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const signupLink = document.querySelector('.signup-link');
@@ -19,9 +19,20 @@ popBtn.addEventListener('click',() => {
 closeBtn.addEventListener('click',() => {
      wrapper.classList.remove('active-popup');  
 });
+///slider
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
 
-
-
-
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
